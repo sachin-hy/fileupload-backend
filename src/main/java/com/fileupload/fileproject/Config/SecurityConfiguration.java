e0fee8e9eab4bb373edf3395a9f6815a8b13303a
sachin-hy <sachin1307saini@gmail.com>
2026-07-20 15:50:16 +0530

    added froentend url

--- src/main/java/com/fileupload/fileproject/Config/SecurityConfiguration.java
+++ src/main/java/com/fileupload/fileproject/Config/SecurityConfiguration.java
@@ -79,7 +79,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://fileupload-frontend.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://filesharing-froentend-1.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
